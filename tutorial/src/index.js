f2becb07d6250318505db5164fe2e4cbc4a4cbac
arleyribeiro <arley.sribeiro@gmail.com>
2021-08-29 14:28:01 -0300

    props - spread operator

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,34 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// Props - properties
 
-const firstBook = {
-  author: "Old Farmer’s Almanac",
-  title:
-    "The Old Farmer's Almanac 2022 Trade EdiThe Old Farmer's Almanac 2022 Trade",
-  img: "https://images-na.ssl-images-amazon.com/images/I/51tpY0oLw3L._AC_SX184_.jpg",
-};
+const books = [
+  {
+    id: 1,
+    author: "Old Farmer’s Almanac",
+    title:
+      "The Old Farmer's Almanac 2022 Trade EdiThe Old Farmer's Almanac 2022 Trade",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51tpY0oLw3L._AC_SX184_.jpg",
+  },
+  {
+    id: 2,
+    author: "1984 (Clássicos da literatura mundial)",
+    title: "George Orwell (Author), Karla Lima (Translater)",
+    img: "https://m.media-amazon.com/images/I/51enL1+QZfL.jpg",
+  },
+];
 
-const secondBook = {
-  author: "1984 (Clássicos da literatura mundial)",
-  title: "George Orwell (Author), Karla Lima (Translater)",
-  img: "https://m.media-amazon.com/images/I/51enL1+QZfL.jpg",
-};
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      >
-        <p>Lorem lorem</p>
-      </Book>
-      <Book {...secondBook} />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
